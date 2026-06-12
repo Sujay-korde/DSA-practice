7c1e8a5daabeb4b2ece3fec1a21fb6ae6084ddd4
Sujay Korde <sujaykorde04@gmail.com>
2026-06-12 10:52:54 +0530

    Time: 0 ms | Memory: 26.1 MB - LeetPush

---
+++ single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,12 @@
+// Single Element in a Sorted Array
+// Difficulty: Medium
+// Runtime: 0 ms
+// Memory: 26.1 MB
+// https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+            }
+        }
+        return nums[low];
+    }
+};
+
